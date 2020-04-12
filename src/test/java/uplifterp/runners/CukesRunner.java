673641a393e33cd4da8a70ae4a8fc4f1537cbe3c
mbalci07 <muratbalci07@gmail.com>
2020-04-12 18:55:58 +0200

    1st day runnerClass

---
+++ src/test/java/uplifterp/runners/CukesRunner.java
@@ -0,0 +1,21 @@
+package uplifterp.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"json:target/cucumber.json",
+                    "html:target/default-html-reports",
+                    "rerun:target/rerun.txt"},
+        features = "src/test/resources/features"
+
+
+
+
+)
+
+public class CukesRunner {
+}
