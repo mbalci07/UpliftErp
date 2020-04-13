630d911069ebfbe6b5eb30ddb56ae821cab4a02e
mbalci07 <muratbalci07@gmail.com>
2020-04-13 11:19:13 +0200

    1st Scnerio

--- src/test/java/uplifterp/runners/CukesRunner.java
+++ src/test/java/uplifterp/runners/CukesRunner.java
@@ -10,10 +10,10 @@ import org.junit.runner.RunWith;
         plugin = {"json:target/cucumber.json",
                     "html:target/default-html-reports",
                     "rerun:target/rerun.txt"},
-        features = "src/test/resources/features"
-
-
-
+        features = "src/test/resources/features",
+        glue = "uplifterp/stepdefs/",
+        dryRun = false,
+        tags = "@wip"
 
 )
 
